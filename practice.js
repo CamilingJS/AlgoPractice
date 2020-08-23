2b209fb38f1149a004e74f5b0fd3c9fe8046ce16
Jonathan <jonathancamiling@gmail.com>
2020-08-22 17:32:04 -0700

    javascript practice 1-3

---
+++ practice.js
@@ -0,0 +1,24 @@
+//LAB - JAVASCRIPT PRACTICE
+//PROBLEM 1
+//Create 100 objects -- 15 of these objects should be things that contain other things (fridge, drawer, shoebox, etc...)
+//and contain and array that contains other objects. Store these objects in arrays named to the room these objects are found.
+
+
+
+//PROBLEM 2
+//You are given a string where each character represents a type of sock (s = stripes, w = white, b = black, d = dress, g = gym),
+//Given this string, find how many pairs you can make of each type, and return this value as an object: 
+/*
+{
+    stripes: Number,
+    white: Number,
+    black: Number,
+    dress: Number,
+    gym: Number
+}
+*/
+//PROBLEM 3
+//You are cracking a safe.
+//The safe has a unknown combo.
+//Write a function that takes the combo with each number 0-9, and brute force unlocks the combo.
+//Pretend the function can't know the combo that is passed in
